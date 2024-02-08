1bc8051818c219907f5bcbe442fdb9ab176f3aca
Matheus <matheus.sacg@gmail.com>
2024-02-08 14:46:15 -0300

    fetch api

--- src/app/processos-seletivos/page.jsx
+++ src/app/processos-seletivos/page.jsx
@@ -1,7 +1,32 @@
-export default function ProcessosSeletivos() {
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+}
+
+export default async function ProcessosSeletivos() {
+    const data = await getData()
+
     return (
-        <div className="">
-            <h1>Processos Seletivos</h1>
-        </div>
+        <>
+            <Header />
+                <div className=" flex flex-col items-center justify-center">
+                    <h1 className="text-center mt-10 font-bold text-xl text-green-800">Processos Seletivos</h1>
+                    <ul className="text-center mt-10">
+                        {data.map((todo) => (
+                            <li className="p-2 border-y-2" key={todo.id}> {todo.title} </li>
+                        ))}
+                    </ul>
+                </div>
+            <Footer />
+        </>
     );
+
 }
